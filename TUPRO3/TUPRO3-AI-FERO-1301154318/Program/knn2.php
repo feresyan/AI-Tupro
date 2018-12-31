51d37f218bc77cb9c1c87b1b589bc225e71c47fa
feresyan <feresyan@gmail.com>
2018-12-31 14:13:48 +0700

    Create knn2.php

---
+++ TUPRO3/TUPRO3-AI-FERO-1301154318/Program/knn2.php
@@ -0,0 +1,105 @@
+<?php
+
+	// DATA SET
+	$i=0;
+	if (($handle = fopen("Dataset.csv", "r")) !== FALSE) {
+	    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+	        	$data_set[$i] = $data;
+	        	$i++;
+	    }
+	}
+	fclose($handle);
+
+	//DATA TEST
+	$i=0;
+	if ( ($handle = fopen("Datatest.csv", "r")) !== FALSE) {
+	    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+			$data_test[$i] = $data;
+        	$i++;
+	    }
+	}
+	fclose($handle);
+
+
+	for ($i=0; $i < 1000 ; $i++) { 
+		//echo $i.". ";
+		for ($x=0; $x < 4000; $x++) { 
+			$data_hasil[$x][0] = sqrt( (($data_test[$i][0] - $data_set[$x][0])**2) + (($data_test[$i][1] - $data_set[$x][1])**2) + (($data_test[$i][2] - $data_set[$x][2])**2) + (($data_test[$i][3] - $data_set[$x][3])**2) );
+			$data_hasil[$x][1] = $data_set[$x][4];
+		}
+		
+		$satu = 0;
+		$nol = 0;
+		sort($data_hasil);
+
+		for ($z=0; $z <= 6 ; $z++) { 
+			if ($data_hasil[$z][1] == 1) {
+				$satu = $satu+1;
+			} else {
+				$nol = $nol+1;
+			}
+		}
+		//echo "1 = ".$satu." | "." 0 = ".$nol."<br>";
+
+		if ( $satu > $nol ) {
+			$hoax[$i] = "Ya";
+		} else {
+			$hoax[$i] ="Tidak";
+		}
+
+		$hasil[$i]['Like'] = $data_test[$i][0];
+		$hasil[$i]['Provokasi'] = $data_test[$i][1];
+		$hasil[$i]['Komentar'] = $data_test[$i][2];
+		$hasil[$i]['Emosi'] = $data_test[$i][3];
+		$hasil[$i]['Hoax'] = $hoax[$i];
+
+	}
+
+	$header = array("Like","Provokasi","Komentar","Emosi","Hoax");
+	$fp = fopen("output.csv", "w");
+	fputcsv ($fp, $header, ","," ");
+	foreach($hasil as $row)
+	{
+	    fputcsv($fp, $row,","," ");
+ 	}
+	fclose($fp);
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TUPRO 3 AI </title>
+		<style type="text/css">
+		td {
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<h1>DATA TEST</h1>
+	<table border="1px solid black" width="40%">
+	  <tr>
+	  	<th>Berita</th>
+	  	<th>Like</th>
+	    <th>Provokasi</th>
+	    <th>Komentar</th> 
+	    <th>Emosi</th>
+	    <th>Hoax</th>
+	  </tr>
+
+	  <?php 
+	  	for ($i=0; $i < 1000 ; $i++) { 
+	  		echo "<tr >
+	  				<td>B $i </td>
+	  				<td>".$data_test[$i][0]."</td>
+				    <td>".$data_test[$i][1]."</td>
+				    <td>".$data_test[$i][2]."</td> 
+				    <td>".$data_test[$i][3]."</td>
+				    <td>".$hoax[$i]."</td>
+				  </tr>";
+	  	}
+	  ?>
+	</table>
+</body>
+</html>
